1bc0f4fad59dc30d59fab9a0a4ab8f203ddda188
Maan R <90124016+MaanAlHababi@users.noreply.github.com>
2024-07-04 15:52:01 +0300

    Add files via upload

---
+++ article-preview-component-master/script.js
@@ -0,0 +1,21 @@
+const shareButton = document.getElementById('share-button');
+const shareMenu = document.getElementById('share-menu');
+const publisherSection = document.getElementById('publisher');
+
+
+shareButton.addEventListener('click', () => {
+    shareButton.classList.toggle('dark');
+    if (window.innerWidth <= 768) {
+        publisherSection.classList.toggle('hidden');
+        shareMenu.classList.toggle('hidden');
+    } else {
+        shareMenu.classList.toggle('hidden');
+    }
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 768 && !shareMenu.classList.contains('hidden')) {
+        shareButton.classList.toggle('dark');
+        shareMenu.classList.toggle('hidden');
+    }
+});
